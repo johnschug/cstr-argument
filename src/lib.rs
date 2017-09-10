4a167be17ce49873e85f9d65ef9084683be63368
John Schug <john.ips.schug@gmail.com>
2017-09-09 20:23:59 -0700

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+// #![cfg_attr(any(nightly, feature = "nightly"), feature(specialization))]
+// #[macro_use]
+// extern crate cfg_if;
+
 use std::borrow::Cow;
 use std::ffi::{CStr, CString};
 use std::result;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct NulError<T> {
     inner: T,
     pos: usize,
@@ -26,6 +30,17 @@ pub trait CStrArgument: Sized {
     fn into_cstr(self) -> Result<Self::Output, Self>;
 }
 
+// BUG in rustc (#23341)
+// cfg_if! {
+//     if #[cfg(any(nightly, feature = "nightly"))] {
+//         impl<T> CStrArgument for T where Self: AsRef<CStr> {
+//             default type Output = Self;
+//
+//             default fn into_cstr(self) -> Result<Self, Self> {
+//                 Ok(self)
+//             }
+//         }
+//     } else {
 impl<'a> CStrArgument for CString {
     type Output = Self;
 
@@ -49,8 +64,10 @@ impl<'a> CStrArgument for &'a CStr {
         Ok(self)
     }
 }
+// }
+// }
 
-impl<'a> CStrArgument for String {
+impl CStrArgument for String {
     type Output = CString;
 
     fn into_cstr(self) -> Result<Self::Output, Self> {
@@ -140,5 +157,119 @@ impl<'a> CStrArgument for &'a [u8] {
                 None => Ok(Cow::Owned(CString::from_vec_unchecked(self.into()))),
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{CStrArgument, NulError};
+
+    fn test<T, F, R>(t: T, f: F) -> R
+    where T: CStrArgument, F: FnOnce(Result<T::Output, NulError<T>>) -> R {
+        f(t.into_cstr())
+    }
+
+    #[test]
+    fn test_basic() {
+        let case = "";
+        test(case, |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len() + 1);
+            assert_ne!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+
+        test(case.to_owned(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len() + 1);
+        });
+
+        test(case.as_bytes(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len() + 1);
+            assert_ne!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+
+        let case = "hello";
+        test(case, |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len() + 1);
+            assert_ne!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+
+        test(case.to_owned(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len() + 1);
+        });
+
+        test(case.as_bytes(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len() + 1);
+            assert_ne!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+    }
+
+    #[test]
+    fn test_terminating_null() {
+        let case = "\0";
+        test(case, |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len());
+            assert_eq!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+
+        test(case.to_owned(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len());
+        });
+
+        test(case.as_bytes(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len());
+            assert_eq!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+
+        let case = "hello\0";
+        test(case, |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len());
+            assert_eq!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+
+        test(case.to_owned(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len());
+        });
+
+        test(case.as_bytes(), |s| {
+            let s = s.unwrap();
+            assert_eq!(s.to_bytes_with_nul().len(), case.len());
+            assert_eq!(s.as_ptr() as *const u8, case.as_ptr());
+        });
+    }
+
+    #[test]
+    fn test_interior_null() {
+        let case = "\0\0";
+        test(case, |s| s.unwrap_err());
+        test(case.to_owned(), |s| s.unwrap_err());
+        test(case.as_bytes(), |s| s.unwrap_err());
+
+        let case = "hello\0world";
+        test(case, |s| s.unwrap_err());
+        test(case.to_owned(), |s| s.unwrap_err());
+        test(case.as_bytes(), |s| s.unwrap_err());
+    }
+
+    #[test]
+    fn test_interior_and_terminating_null() {
+        let case = "hello\0world\0";
+        test(case, |s| s.unwrap_err());
+        test(case.to_owned(), |s| s.unwrap_err());
+        test(case.as_bytes(), |s| s.unwrap_err());
+
+        let case = "hello world\0\0";
+        test(case, |s| s.unwrap_err());
+        test(case.to_owned(), |s| s.unwrap_err());
+        test(case.as_bytes(), |s| s.unwrap_err());
     }
 }
